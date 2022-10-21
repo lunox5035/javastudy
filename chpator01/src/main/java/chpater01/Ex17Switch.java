ac39dfb6c2f613662b945aebb3f9a65d309d506c
GawnGyu Yeo <bbc02131@naver.com>
2022-10-21 14:40:49 +0900

    기본 프로그래밍#수정

--- chpator01/src/main/java/chpater01/Ex17Switch.java
+++ chpator01/src/main/java/chpater01/Ex17Switch.java
@@ -9,14 +9,16 @@ public class Ex17Switch {
 			case 'A': 
 			case 'B':{
 				System.out.println( "참 잘어요");
+				break;
 			}
-			case 'C: 
+			case 'C': 
 			case 'D':{
-				System.out.println( "좀 더 노력하세요");
-
+				System.out.println("좀 더 노력하세요");
+				break;
+			}
 			case 'F':{
 				System.out.println( "재수강합니다;");
-			yield type;
+				break;
 		}
 		default:
 			System.out.println( "잘못 된 학점입니다.;");
