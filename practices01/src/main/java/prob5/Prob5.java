5defbbceb59128c95adbfc0108b853f19e5282f0
GawnGyu Yeo <bbc02131@naver.com>
2022-10-21 17:50:33 +0900

    not done

--- practices01/src/main/java/prob5/Prob5.java
+++ practices01/src/main/java/prob5/Prob5.java
@@ -3,5 +3,29 @@ package prob5;
 public class Prob5 {
 
 	public static void main(String[] args) {
+				
+		//반복문 100까지 
+		for (int a=1; 1<=99; a++) {
+			int count=0;
+						
+			String number = String.valueOf(a);			//형변환 int=>String			
+
+			//숫자 내 3,6,9 갯수 세기
+			for (int e = 0; e<number.length(); e++) {	
+				char c = number.charAt(e);
+
+				if (c==3||c==6||c==9) {					
+					count++;							
+				}
+			}
+
+			//출력문
+			System.out.print(a);
+			for (int z =0;z<count;a++) {
+				System.out.print("짝");
+			}	
+		}
 	}
 }
+
+	
