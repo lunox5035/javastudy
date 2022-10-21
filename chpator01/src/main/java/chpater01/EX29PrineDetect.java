e41ead8755ba96d3633598c1f4a6d6dd37ef62ed
GawnGyu Yeo <bbc02131@naver.com>
2022-10-21 17:20:58 +0900

    fix

--- chpator01/src/main/java/chpater01/EX29PrineDetect.java
+++ chpator01/src/main/java/chpater01/EX29PrineDetect.java
@@ -4,19 +4,24 @@ public class EX29PrineDetect {
 
 	public static void main(String[] args) {
 		// 2~1000 까지에서 소수를 화면에 출력해세요
-		
-//		boolean isprime = true;
-//		int num=1000;
-		
-		
-		
-		
-		while () {
-			if () {
-				System.out.println();
+
+//		int num=5;
+
+		int i = 2;
+		while (i < 1000){
+
+			boolean isprime = true;
+			for (int num = 2; num < i; num++) {
+				if (i % num == 0) {
+					isprime = false;
+					break;
+				}
 			}
-			
-			
+			if (isprime==true) {
+				System.out.println(i);
+			}
+			i++;
+
 		}
 
 	}
