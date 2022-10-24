ebb8a3af0e6f5988e31a8e120c3ddec33dc67bce
GawnGyu Yeo <bbc02131@naver.com>
2022-10-24 11:15:34 +0900

    기본프로그래밍 - 배열 #

---
+++ chapter01/src/main/java/chpater01/MainParam.java
@@ -0,0 +1,10 @@
+package chpater01;
+
+public class MainParam {
+
+		public static void main(String[] args) {
+			for(int i = 0; i < args.length; i++) {
+				System.out.println(args[i]);
+			}
+		}
+}
