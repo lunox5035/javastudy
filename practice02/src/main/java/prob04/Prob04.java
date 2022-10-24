cbc6b4ee95a4c2c4dbcb1a568423272c0ea374ac
GawnGyu Yeo <bbc02131@naver.com>
2022-10-24 17:02:45 +0900

    not done

--- practice02/src/main/java/prob04/Prob04.java
+++ practice02/src/main/java/prob04/Prob04.java
@@ -2,20 +2,34 @@ package prob04;
 public class Prob04 {
 
 	public static void main(String[] args) {
-		char[] c1 = reverse( "Hello World" );
-		printCharArray( c1 );
+		char[] c1 = reverse("Hello World");
+		printCharArray(c1);
 		
-		char[] c2 = reverse( "Java Programming!" );
-		printCharArray( c2 );
+		char[] c2 = reverse("Java Programming!");
+		printCharArray(c2);
 	}
 	
 	public static char[] reverse(String str) {
-		/* 코드를 완성합니다 */
+		char[] cs = str.toCharArray();
+		
 		return null;
 	}
 
 	public static void printCharArray(char[] array){
 		/* 코드를 완성합니다 */
-		System.out.println( array );
+		
+		String reverse = "";
+		for (int i=array.length-1;i>0;i--) {
+			reverse=reverse+array[i];
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println(reverse);
 	}
 }
