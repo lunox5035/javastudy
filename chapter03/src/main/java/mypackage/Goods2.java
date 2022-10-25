f8aa1d77d0a14554cfd2b5fdf730cab62b2b19db
GawnGyu Yeo <bbc02131@naver.com>
2022-10-25 14:16:29 +0900

    객체지향프로그래밍 #2

--- chapter03/src/main/java/mypackage/Goods2.java
+++ chapter03/src/main/java/mypackage/Goods2.java
@@ -4,7 +4,7 @@ public class Goods2 {
 	public String name;			//모든 접근이 가능
 	protected int price;		//같은 페키지+자식접근
 	int countStock;				//디폴트,같은 패키지
-	private int countSold;	//클래스 내부에서만 접근가능
+	private int countSold;		//클래스 내부에서만 접근가능
 	
 	public void m() {
 		countSold=100;
