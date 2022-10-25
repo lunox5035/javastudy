0799695fdcd1b6b227d01a718cf848df36de75f6
GawnGyu Yeo <bbc02131@naver.com>
2022-10-25 17:27:58 +0900

    .

--- chapter03/src/main/java/chapter03/GoodsApp.java
+++ chapter03/src/main/java/chapter03/GoodsApp.java
@@ -3,20 +3,20 @@ package chapter03;
 public class GoodsApp {
 	
 	public static void main(String[] args) {
-		Goods camera = new Goods();	//┓		//기본생성자
-		camera.setName("nikon");	//┃
-		camera.setPrice(40000);		//┣━필드
-		camera.setCountStock(30);	//┃
-		camera.setCountSold(50);	//┃
-		System.out.println(camera);	//┃
+		Goods camera = new Goods();		//기본생성자
+		camera.setName("nikon");	
+		camera.setPrice(40000);		
+		camera.setCountStock(30);	
+		camera.setCountSold(50);	
+		System.out.println(camera);	
 		camera.showInfo();
 		
 		int discouintPrice = camera.calcDiscouintPrice(0.5);
 		System.out.println("Discount Price"+discouintPrice);
-									//┃
-		Goods g1 = new Goods();		//┃
-		Goods g2 = new Goods();		//┃
-		Goods g3 = new Goods();		//┛
+									
+		Goods g1 = new Goods();		
+		Goods g2 = new Goods();		
+		Goods g3 = new Goods();		
 		
 		
 		System.out.println(Goods.countOfGoods);
