7f0960964220f13b625c245f2f9c4e241fc4169b
GawnGyu Yeo <bbc02131@naver.com>
2022-10-26 14:17:02 +0900

    그림판#2

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -4,11 +4,13 @@ public class Main {
 	
 	public static void main(String[] args) {
 	
-		Point pt = new Point();
-		pt.setX(10);
-		pt.setY(50);
+		Point pt = new Point(10,50);
+//		pt.setX(10);
+//		pt.setY(50);
 		
 		drawPonint(pt);
+//		pt.disappear();
+		pt.show(false);
 	}
 	
 	public static void drawPonint(Point pt) {
--- chapter03/src/main/java/paint/Point.java
+++ chapter03/src/main/java/paint/Point.java
@@ -4,6 +4,15 @@ public class Point {
 	private int x;
 	private int y;
 	
+	public Point() {
+		
+	}
+		
+	public Point(int x,int y) {
+		this.x=x;
+		this.y=y;
+	}
+	
 	public int getX() {
 		return x;
 	}
@@ -18,11 +27,23 @@ public class Point {
 		this.y = y;
 	}
 	
-	
-	public void show() {
-		System.out.println("점(x="+x+", y="+y+"_를 그렸습니다.");
+	public void show(boolean visible) {
+		if(visible) {
+			show();
+		}else {
+			System.out.println("점(x="+x+", y="+y+"를 지웠습니다.");
+		}
 	}
 	
+	
+	public void show() {
+		System.out.println("점(x="+x+", y="+y+"를 그렸습니다.");
+	}
+	
+//	public void disappear() {
+//		System.out.println("점(x="+x+", y="+y+"를 지웠습니다.");
+//	}
+//	
 	
 	
 	
