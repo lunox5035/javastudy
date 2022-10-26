47ad34e33e58f395bd2111b711677d3430da1268
GawnGyu Yeo <bbc02131@naver.com>
2022-10-26 15:26:41 +0900

    다형성 오버라이딩

--- chapter03/src/main/java/paint/Main.java
+++ chapter03/src/main/java/paint/Main.java
@@ -4,16 +4,35 @@ public class Main {
 	
 	public static void main(String[] args) {
 	
-		Point pt = new Point(10,50);
+		Point pt1 = new Point(10,50);
 //		pt.setX(10);
 //		pt.setY(50);
 		
-		drawPonint(pt);
-//		pt.disappear();
-		pt.show(false);
+		drawPonint(pt1);
+//		pt1.disappear();
+		pt1.show(false);
+	
+		Point pt2= new ColorPoint();
+		pt2.setX(100);
+		pt2.setY(200);
+		((ColorPoint)pt2).setColor("red");
+
+		drawPonint(pt2);		
+		
+//		ColorPoint pt2 = new ColorPoint();
+//		pt2.setX(100);
+//		pt2.setY(200);
+//		pt2.setColor("red");
+
+//		drawColorPoint(pt2);
 	}
 	
 	public static void drawPonint(Point pt) {
 		pt.show();
 	}
+
+//	public static void drawCorlorPoint(ColorPoint pt) {
+//		pt.show();
+//	}
+	
 }
