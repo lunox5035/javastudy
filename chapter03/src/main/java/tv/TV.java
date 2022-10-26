6f0c7d39ca21cd98735860bcfa7a0abcf60fccca
GawnGyu Yeo <bbc02131@naver.com>
2022-10-26 14:15:52 +0900

    TV#1

---
+++ chapter03/src/main/java/tv/TV.java
@@ -0,0 +1,15 @@
+package tv;
+
+public class TV {
+	private int channel;	//1~255 사이값을 가지게 하고 넘어 갈때는 라인딩 시킬 것
+	private int volume;		//0~100 사이값을 가지게 하고 넘어 갈때는 라인딩 시킬 것
+	private boolean power;	
+	
+	public void status() {
+		System.out.println(
+				"TV[channel="	+channel+
+				", volume="		+volume+
+				", power"		+(power ? "on":"off")+"]");
+	}
+	
+}
