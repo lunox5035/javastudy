399871b7983b82be7609327dae6ad124d6ab0d5b
GawnGyu Yeo <bbc02131@naver.com>
2022-10-28 20:31:55 +0900

    notdone

--- practice03/src/main/java/prob05/Account.java
+++ practice03/src/main/java/prob05/Account.java
@@ -1,4 +1,49 @@
 package prob05;
 
 public class Account {
+	
+	private String accountNo;
+	private int balance;
+	private String account;
+		
+	//메소드
+
+	public Account(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+	
+	public String getAccString() {
+		return account;
+	}
+		
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public void setAccount(String string) {
+		this.account=account;
+	}
+	
+	public int save(int i) {
+		return balance-i;
+		
+	}
+	
+	public int deposit(int i) {
+		return balance+i;
+		
+	}
+
 }
