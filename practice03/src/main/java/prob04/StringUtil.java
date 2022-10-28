5c292db465211707e50a10b44a4013c883941cc4
GawnGyu Yeo <bbc02131@naver.com>
2022-10-28 17:58:56 +0900

    done

--- practice03/src/main/java/prob04/StringUtil.java
+++ practice03/src/main/java/prob04/StringUtil.java
@@ -1,4 +1,12 @@
 package prob04;
 
 public class StringUtil {
+
+	public static String concatenate(String[] strArr) {
+		String resultStr="";
+		for (int i =0;i<strArr.length;i++) {
+			resultStr=resultStr+strArr[i];
+		}
+		return resultStr;
+	}
 }
